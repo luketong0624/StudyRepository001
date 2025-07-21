589b2da38abbbc076d2f96d1f5dd57c491f7b9f8
童晓军 <358752247@qq.com>
2025-07-21 10:20:54 +0800

    第一次提交到仓库

---
+++ 51cto/ui.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import streamlit as st
+
+#示例：数据可视化
+# 上传 xlsx 文件
+uploaded_file =st.file_uploader("上传数据",type='xlsx')
+if uploaded_file:
+    #读取Excel文件
+    excel_file=pd.ExcelFile(uploaded_file)
+    #获取第一个工作表的数据
+    df=excel_file.parse(excel_file.sheet_names)
+
+    #显示数据的前几行
+    st.dataframe(df.head())
+
+    #查找包含”销售数据“的列
+    column_name=None
+    for col in df.columns:
+        if "销售数量" in  col:
+            column_name=col
+            break
+
+    if column_name:
+        # 生成“销售数量”列的折线图
+        st.line_chart(df[column_name])
+    else:
+        st.warning("数据中未找到包含 '销售数量' 的列，无法生成图表。")
